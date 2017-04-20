4a94a2878907395ada822e5511006b9e3f975633
mehakdua <mdua798@gmail.com>
2017-04-20 15:24:10 +0530

    commit express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require("express");
 var app = express();
 app.use(express.static(__dirname+"/build"));
 app.get("*",function(req,res) {
-req.sendFile("./build/index.html");
+req.sendfile("./build/index.html");
 });
 var port = process.env.PORT|9000
 app.listen(port);
